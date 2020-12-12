510ce0b65d9049ddee2bd459231c7bb1aa7f3b8e
FaustoMonte <fausto_montenegro@hotmail.com>
2020-12-12 10:16:10 -0500

    Quinto commit se instala flask_mysql para acceso a datos

--- App.py
+++ App.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import json
 #import flask_mysqldb
+from flask_mysqldb import MySQL
 from flask_cors import CORS
 import os
 import uuid
@@ -11,7 +12,21 @@ from werkzeug.utils import secure_filename
 UPLOAD_FOLDER = "./uploads"
 ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
 
+#filter mime-types
+def allowed_files(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
 app = Flask(__name__)
+
+@app.route("/api/posts", methods=["GET"])
+def index():
+    if request.method == "GET":
+        return jsonify(data="posts main response")
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
