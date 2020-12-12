7375445ba45af64bf16563769442dd8399243ac7
FaustoMonte <fausto_montenegro@hotmail.com>
2020-12-12 08:45:49 -0500

    Tercer commit se configura la carpeta local temporal para subir imagenes

--- App.py
+++ App.py
@@ -1,4 +1,17 @@
 from flask import Flask, request, jsonify
 import json
-#flask_mysqldb
-#  
+#import flask_mysqldb
+from flask_cors import CORS
+import os
+import uuid
+from werkzeug.utils import secure_filename
+#import firebase
+
+#local uploads or temp
+UPLOAD_FOLDER = "./uploads"
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
+
+app = Flask(__name__)
+
+if __name__ == "__main__"
+    app.run()
