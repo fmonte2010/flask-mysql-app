a653a8bf639e7ab4696b46024af43bdf15b76ba9
FaustoMonte <fausto_montenegro@hotmail.com>
2020-12-13 09:49:36 -0500

    Noveno commit se generan el servicio flaskapi, los componentes main, addposts y post

--- angularposts/src/app/app.module.ts
+++ angularposts/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { AddpostsComponent } from './addposts/addposts.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    AddpostsComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
